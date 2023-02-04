253c9cbd4b856aae324530fc4f99320b1becac88
M Bilal <bilaldarmuhammad@gmail.com>
2023-02-04 21:44:24 +0500

    props

---
+++ src/components/ButtonCmp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ButtonCmp(props) {
+  //JavaScript
+
+  return (
+    //HTML
+    <div>
+      <button>{props.btn}</button>
+    </div>
+  );
+}
+
+export default ButtonCmp;
