afb44f2fe66a5c695ff79be2803f1c565ad0e35f
M Bilal <bilaldarmuhammad@gmail.com>
2023-02-04 22:04:55 +0500

    Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+//React Router Dom
+//npm i react-router-dom
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <ButtonCmp btn='Show me' />
+      <h1>Hello world</h1>
+      <ButtonCmp btn='Click me' /> */}
+      {/* <State /> */}
+      <Router>
+        <Routes>
+          <Route path='/' element={< Home />} />
+          <Route path='/Login' element={< Login />} />
+        </Routes>
+      </Router>
+    </>
+
+
   );
 }
 
