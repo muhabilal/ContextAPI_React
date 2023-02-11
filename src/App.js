8a2a62a29315bb26c03060777d24fa1465dc029f
M Bilal <bilaldarmuhammad@gmail.com>
2023-02-11 15:14:34 +0500

    import useContext in app

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/Login';
-
+import NoteState from './context/notes/NoteState';
+import NavBar from './pages/NavBar';
 //React Router Dom
 //npm i react-router-dom
 function App() {
   return (
     <>
-      {/* <ButtonCmp btn='Show me' />
-      <h1>Hello world</h1>
-      <ButtonCmp btn='Click me' /> */}
-      {/* <State /> */}
-      <Router>
-        <Routes>
-          <Route path='/' element={< Home />} />
-          <Route path='/Login' element={< Login />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Routes>
+            <Route path='/' element={< Home />} />
+            <Route path='/NavBar' element={< NavBar />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
 
 
